8baa11f3499dcffd0a1f965bbd1b9f67e64f87d2
Damien Cortina <damien.cortina@gmail.com>
2024-01-27 20:38:10 +0100

    ➕ Add typescript support

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 module.exports = {
+    mode: 'production',
     entry: './src/app.js',
     output: {
         filename: 'retro_fps.js',
@@ -11,8 +12,15 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
-            }
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
         ]
-    }
-
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
 }
